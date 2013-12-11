1c364087f1bae7cf2bd35e5ef20489ea726770f3
Gwenaël Pluchon <accounts@gwenp.fr>
2013-12-11 22:21:02 +0100

    apply bookmarks patch

--- config.def.h
+++ config.def.h
@@ -28,7 +28,8 @@ static Bool allowgeolocation = TRUE;
 
 #define SETPROP(p, q) { \
 	.v = (char *[]){ "/bin/sh", "-c", \
-		"prop=\"`xprop -id $2 $0 | cut -d '\"' -f 2 | xargs -0 printf %b | dmenu`\" &&" \
+		"prop=\"`(xprop -id $2 $0 | cut -d '\"' -f 2 | xargs -0 printf %b && "\
+		"cat ~/.surf/bookmarks) | dmenu`\" &&" \
 		"xprop -id $2 -f $1 8s -set $1 \"$prop\"", \
 		p, q, winid, NULL \
 	} \
@@ -43,6 +44,12 @@ static Bool allowgeolocation = TRUE;
 		d, useragent, r, cookiefile, NULL \
 	} \
 }
+
+#define BM_ADD { .v = (char *[]){ "/bin/sh", "-c", \
+	"(echo `xprop -id $0 _SURF_URI | cut -d '\"' -f 2` && "\
+	"cat ~/.surf/bookmarks) | awk '!seen[$0]++' > ~/.surf/bookmarks_new && "\
+	"mv ~/.surf/bookmarks_new ~/.surf/bookmarks", \
+	winid, NULL } }
 
 #define MODKEY GDK_CONTROL_MASK
 
@@ -75,6 +82,7 @@ static Key keys[] = {
     { MODKEY,               GDK_space,       scroll_v,   { .i = +10000 } },
     { MODKEY,               GDK_i,           scroll_h,   { .i = +1 } },
     { MODKEY,               GDK_u,           scroll_h,   { .i = -1 } },
+    { MODKEY,               GDK_b,      spawn,      BM_ADD },
 
     { 0,                    GDK_F11,    fullscreen, { 0 } },
     { 0,                    GDK_Escape, stop,       { 0 } },
